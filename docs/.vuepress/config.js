e03a3fe483228985facbaefa5589e96c1375b713
Shapovalov Andrii <shapovalov.andrii@lll.kpi.ua>
2024-02-21 08:42:52 +0200

    update repo URL in config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -74,7 +74,7 @@ module.exports = {
 
     lastUpdated: 'Останнє оновлення', // string | boolean
       // Assumes GitHub. Can also be a full GitLab url.
-    repo: 'https://github.com/boldak/edu-dis-labs',
+    repo: 'https://github.com/nonGratis/retarder-io-23-3.git',
     // Customising the header label
     // Defaults to "GitHub"/"GitLab"/"Bitbucket" depending on `themeConfig.repo`
     repoLabel: 'Github',
